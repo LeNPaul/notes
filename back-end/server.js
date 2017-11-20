569d93e3244e121d4723d9433717e91ee33b0781
Paul Le <l.nguyen.paul@gmail.com>
2017-11-19 23:03:11 -0500

    Add mongo database transactions

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
+
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+})
 
 app.post('/api/message', function(req,res){
   console.log(req.body);
   res.status(200);
+})
+
+mongo.connect("mongodb://localhost:27017/test", function(err,db){
+  if(!err){
+    console.log("we are conected to mongo");
+
+    db.collection('messages').insertOne({'msg':'test'});
+
+  }
 })
 
 var server = app.listen(5000,function(){
