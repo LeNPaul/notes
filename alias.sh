6c005149bfabd09538d75b9eef9925499be5cc1f
Paul Le <l.nguyen.paul@gmail.com>
2019-02-19 23:48:24 -0500

    Add common aliases used.

---
+++ alias.sh
@@ -0,0 +1,7 @@
+alias github="cd ~/Documents/Github/"
+alias gowork="cd ~/Documents/iNTERFACEWARE/"
+
+alias ppcology="github; cd ppcology; atom ../ppcology;"
+
+alias start-iguana="cd /Users/paul.le/Documents/iNTERFACEWARE/Iguana/iguana_6_1;./iguana_service;"
+alias stop-iguana="sudo pkill -SIGTERM iguana_service"
