efaf3a3caa7a587533aa637c412f072b3b9bf49c
Paul Le <l.nguyen.paul@gmail.com>
2019-02-20 23:51:23 -0500

    Add script for starting Physics in Motion.

--- alias.sh
+++ alias.sh
@@ -2,6 +2,7 @@ alias github="cd ~/Documents/Github/"
 alias gowork="cd ~/Documents/iNTERFACEWARE/"
 
 alias ppcology="github; cd ppcology; atom ../ppcology;"
+alias pim="github; cd physics-in-motion; atom ../physics-in-motion;"
 
 alias start-iguana="cd /Users/paul.le/Documents/iNTERFACEWARE/Iguana/iguana_6_1;./iguana_service;"
 alias stop-iguana="sudo pkill -SIGTERM iguana_service"
