43577efca5c5efafce1a86610af0e72a3e3cb498
Paul Le <l.nguyen.paul@gmail.com>
2019-02-22 23:44:52 -0500

    Add alias for viewing Iguana processes running.

--- alias.sh
+++ alias.sh
@@ -5,4 +5,5 @@ alias ppcology="github; cd ppcology; atom ../ppcology;"
 alias pim="github; cd physics-in-motion; atom ../physics-in-motion;"
 
 alias start-iguana="cd /Users/paul.le/Documents/iNTERFACEWARE/Iguana/iguana_6_1;./iguana_service;"
-alias stop-iguana="sudo pkill -SIGTERM iguana_service"
+alias stop-iguana="sudo pkill -SIGTERM iguana_service;"
+alias show-iguana="ps aux | grep iguana;"
