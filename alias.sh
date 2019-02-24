3d0ff361458c311e3655086ef374994e8279be0d
Paul Le <l.nguyen.paul@gmail.com>
2019-02-23 23:51:48 -0500

    Update PPCology command.

--- alias.sh
+++ alias.sh
@@ -1,7 +1,7 @@
 alias github="cd ~/Documents/Github/"
 alias gowork="cd ~/Documents/iNTERFACEWARE/"
 
-alias ppcology="github; cd ppcology; atom ../ppcology;"
+alias ppcology="github; cd ppcology; atom ../ppcology; ssh ifw@ppcology;"
 alias pim="github; cd physics-in-motion; atom ../physics-in-motion;"
 
 alias start-iguana="cd /Users/paul.le/Documents/iNTERFACEWARE/Iguana/iguana_6_1;./iguana_service;"
